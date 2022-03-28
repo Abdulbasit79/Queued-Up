e8cea8de8f981a4d90e0a97b0a6a50e07cc8f8bc
Abdulmajeed Sabriye <abdulmajeed.sabriye777@gmail.com>
2022-03-28 19:08:06 -0400

    main functionality fixed

--- watchlist/src/App.js
+++ watchlist/src/App.js
@@ -1,37 +1,34 @@
-import React from "react"
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import {Header}  from "./components/Header"
-import {WatchList} from "./components/WatchList.js"
-import {Watched} from "./components/Watched.js"
-import {Add} from "./components/Add.js"
-import "./lib/font-awesome/css/all.min.css" 
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Watchlist } from "./components/Watchlist";
+import { Watched } from "./components/Watched";
+import { Add } from "./components/Add";
+import "./App.css";
+import "./lib/font-awesome/css/all.min.css";
+
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-   <Router>
-    <Header/>
+    <GlobalProvider>
+      <Router>
+        <Header />
 
-
-    <Switch> 
-      <Route exact path = "/">
-        <WatchList/>
-      </Route>
-
-      <Route path= "/Watched">
-        <Watched/>
-
-      </Route>
-     
-      <Route path= "/add">
-        <Add/>
-
-      </Route>
-    </Switch>
-   </Router>
-
-    );
+        <Switch>
+          <Route exact path="/">
+            <Watchlist />
+          </Route>
+          <Route path="/add">
+            <Add />
+          </Route>
+          <Route path="/watched">
+            <Watched />
+          </Route>
+        </Switch>
+      </Router>
+    </GlobalProvider>
+  );
 }
 
 export default App;
-  
